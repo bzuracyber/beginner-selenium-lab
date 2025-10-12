8bec35ed2143b6823093fdee1e925f0351519024
bzuracyber <dannybzura@gmail.com>
2025-10-12 14:15:58 -0400

    Updated Selenium Tests

--- selenium-tests/test_invalid_login.py
+++
@@ -1,6 +0,0 @@
-def test_invalid_login(driver):
-    driver.get("http://localhost:3000/#/login")
-    driver.find_element("id", "email").send_keys("fake@user.com")
-    driver.find_element("id", "password").send_keys("wrongpass")
-    driver.find_element("id", "loginButton").click()
-    assert "Invalid email or password" in driver.page_source
--- selenium-tests/test_navigation.py
+++
@@ -1,4 +0,0 @@
-def test_navigation(driver):
-    driver.get("http://localhost:5000/")
-    driver.find_element("link text", "Login").click()
-    assert "Login" in driver.page_source
