796d361cc8ad578a1695987ec6e9f84e344a44f8
bzuracyber <dannybzura@gmail.com>
2025-10-12 14:13:05 -0400

    localhost

--- selenium-tests/test_form_validation.py
+++
@@ -1,4 +0,0 @@
-def test_contact_form_validation(driver):
-    driver.get("http://localhost:3000/#/contact")
-    driver.find_element("id", "submitButton").click()
-    assert "Please provide an email address" in driver.page_source
--- selenium-tests/test_login.py
+++
@@ -1,9 +0,0 @@
-def test_login(driver):
-    driver.get(driver.base_url)
-    # Example login flow
-    driver.find_element("id", "navbarAccount").click()
-    driver.find_element("id", "navbarLoginButton").click()
-    driver.find_element("id", "email").send_keys("admin@juice-sh.op")
-    driver.find_element("id", "password").send_keys("admin123")
-    driver.find_element("id", "loginButton").click()
-    assert "account" in driver.page_source.lower()
